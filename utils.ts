f3243345bb44be0581f120c4d55fa63c99dd7329
colinzappia <colinzappia@gmail.com>
2026-06-10 22:37:36 -0400

    Create utils.ts

---
+++ utils.ts
@@ -0,0 +1,8 @@
+export const getPeriodLabel = (p: number): string => {
+  if (p === 1) return '1st';
+  if (p === 2) return '2nd';
+  if (p === 3) return '3rd';
+  if (p === 4) return 'OT';
+  if (p >= 5) return `OT${p - 3}`;
+  return String(p);
+};
