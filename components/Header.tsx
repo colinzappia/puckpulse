fc43c06f2983893075c58ab529a28f59c2c53182
colinzappia <colinzappia@gmail.com>
2026-06-10 22:26:53 -0400

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,13 @@
 import { UserButton } from '@clerk/clerk-react';
 import React, { useState, useRef, useEffect } from 'react';
-import { getPeriodLabel } from '../App';
+const getPeriodLabel = (p: number) => {
+  if (p === 1) return '1st';
+  if (p === 2) return '2nd';
+  if (p === 3) return '3rd';
+  if (p === 4) return 'OT';
+  if (p >= 5) return \`OT\${p - 3}\`;
+  return \`\${p}\`;
+};
 
 interface TeamDisplay {
   name: string;
