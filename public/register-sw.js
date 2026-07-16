1eb002fd98f18f50bf5fad3ecdff20d97ab15d05
colinzappia <colinzappia@gmail.com>
2026-07-16 15:43:28 -0400

    Update register-sw.js

--- public/register-sw.js
+++ public/register-sw.js
@@ -1,4 +1,4 @@
-if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator && location.hostname !== 'localhost' && location.hostname !== '127.0.0.1') {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js').then(reg => {
       console.log('TCH Service Worker registered:', reg.scope);
