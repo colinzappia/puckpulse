cfe508dad3a6b65ea1a32d53243c83eafb0061bb
colinzappia <colinzappia@gmail.com>
2026-08-14 14:36:40 -0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1917,6 +1917,7 @@ const App: React.FC = () => {
     'shahbazimel@gmail.com',
     'patrick.grandmaitre@uottawa.ca',
     'patrickdelislehoude@cunet.carleton.ca',
+    'jboyd@ontariohockeyleague.com'
   ];
   const userEmail = currentUser?.primaryEmailAddress?.emailAddress?.toLowerCase() || user?.primaryEmailAddress?.emailAddress?.toLowerCase() || '';
   const isAdmin = ADMIN_EMAILS.includes(userEmail);
